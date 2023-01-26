c7acf7db9d62eedc628d5d8acc4ca27525d18d42
saracassandra7 <saracassandra7@gmail.com>
2023-01-26 17:17:04 +0100

    [add] milestone 4

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script>
-export default {
-  name: 'App'
-  
-}
-</script>
+import axios from 'axios';
 
+export default {
+  name: 'App',
+  data(){
+    return{
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      projects: []
+    }
+  },
+
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + 'projects')
+      .then(result=>{
+        this.projects = result.data.projects;
+        console.log(result.data.projects)
+      })
+      
+    }
+  },
+
+  mounted(){
+    this.getApi();
+  }
+}
+
+</script>
+  
 
 <template>
-<h1>hello vue</h1>
+<div v-for="project in projects" :key="project.id">
+  <h3>{{project.name}}</h3>
+</div>
 </template>
 
 
